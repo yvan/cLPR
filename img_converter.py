32b0053046d4e75e26ddbd03eb1834ff750a7e62
yvan <yvanscher@gmail.com>
2018-06-09 17:39:30 -0400

    updated imge converter

--- img_converter.py
+++ img_converter.py
@@ -29,19 +29,18 @@ if __name__ == '__main__':
     dim2 = args['dim2']
     format = args['format'].upper()
     ext = format.lower()
-
-    if format == 'JPG':
-        if not os.path.exists('./imgs_jpg'): os.mkdir('imgs_jpg')
-    elif format == 'PNG':
-        if not os.path.exists('./imgs_png'): os.mkdir('imgs_png')
-    elif format == 'BMP':
-        if not os.path.exists('./imgs_bmp'): os.mkdir('imgs_bmp')
+    
+    if format == 'JPG': format = 'JPEG'
 
     img_paths = glob.glob(g)
+    print(g)
+    print(img_paths)
     for img_path in img_paths:
         img = Image.open(img_path)
         path, basename = os.path.split(img_path)
         basename, _ = os.path.splitext(basename)
         if dim1 and dim2:
             img = img.resize((dim1,dim2))
-        img.save(f'{out}/{basename}.{ext}', format=format, subsampling=0, quality=100)
+        saveto = os.path.join(f'{out}', f'{basename}.{ext}')
+        print(f'saving to {saveto}')
+        img.save(saveto, format=format, subsampling=0, quality=100)
